868d438e2db92fbde7f28addf72405327033fa77
Roberto Morales <a01642426@tec.mx>
2025-09-25 20:19:23 -0600

    D&D tool dev fix 1

--- src/SpellsPage.tsx
+++ src/SpellsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import type { Spell, SpellClass, SpellLevel } from "./data/spell-types";
 import { SPELLS } from "./data/spells.db";
 import SpellCard from "./components/spells/SpellCard";
